105b3b477ed090cd1f451d32f79d37f3cbcb77ee
SEOKKAMONI <sj01048260279@gmail.com>
2023-06-03 15:33:37 +0900

    ADD :: Button

---
+++ src/components/common/Button/type.ts
@@ -0,0 +1,12 @@
+export type ButtonOptionType =
+  | "PRIMARY"
+  | "SECONDARY"
+  | "TERTIARY"
+  | "QUATERNARY"
+  | "DELETE"
+  | "LINK_TEXT"
+  | "HOVER_UNDERLINE";
+
+export type ButtonIconType = "NONE" | "ADD_ICON" | "SHORTCUTS_ICON";
+
+export type ButtonSizeType = "LARGE" | "MEDIUM" | "SMALL";
