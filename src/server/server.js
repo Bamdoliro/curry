e711ad36e383b4ca561d70cf3257bcbc2f004b61
SEOKKAMONI <sj01048260279@gmail.com>
2023-06-04 00:06:54 +0900

    ADD :: posts 경로 수정

--- src/server/server.js
+++ src/server/server.js
@@ -12,26 +12,26 @@ app.prepare().then(() => {
   const server = express();
 
   server.get("/api/job/1", (req, res) => {
-    var path = "src/posts/밤돌이로.md";
+    var path = "posts/밤돌이로.md";
     var file = fs.readFileSync(path, "utf8");
     console.log(file);
     res.send(file);
   });
 
   server.get("/api/job/2", (req, res) => {
-    var path = "src/posts/프론트엔드.md";
+    var path = "posts/프론트엔드.md";
     var file = fs.readFileSync(path, "utf8");
     res.send(marked(file.toString()));
   });
 
   server.get("/api/job/3", (req, res) => {
-    var path = "src/posts/백엔드.md";
+    var path = "posts/백엔드.md";
     var file = fs.readFileSync(path, "utf8");
     res.send(marked(file.toString()));
   });
 
   server.get("/api/job/4", (req, res) => {
-    var path = "src/posts/디자이너.md";
+    var path = "posts/디자이너.md";
     var file = fs.readFileSync(path, "utf8");
     res.send(marked(file.toString()));
   });
