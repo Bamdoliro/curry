4f6f87da038edf79dc773c1ca409bec9353bf9b6
SEOKKAMONI <sj01048260279@gmail.com>
2023-06-07 00:19:52 +0900

    FIX :: google form link

--- src/data/JobDetailData.ts
+++ src/data/JobDetailData.ts
@@ -10,25 +10,29 @@ const JOB_DETAIL_DATA: JobDetailDataType[] = [
     post: "곰돌이로.md",
     title: "Gomdoliro",
     position: "곰돌이로 팀원",
-    googleFormLink: "https://www.simblue.kro.kr/application/48",
+    googleFormLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLScBmssFZc5Rxxj5YLxxP2Mq0EKAphbeNCPKBl48XPGWUGFpZQ/viewform",
   },
   {
     post: "프론트엔드.md",
     title: "Frontend Developer",
     position: "프론트엔드 개발자",
-    googleFormLink: "https://www.simblue.kro.kr/application/48",
+    googleFormLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLScBmssFZc5Rxxj5YLxxP2Mq0EKAphbeNCPKBl48XPGWUGFpZQ/viewform",
   },
   {
     post: "백엔드.md",
     title: "Backend Developer",
     position: "백엔드 개발자",
-    googleFormLink: "https://www.simblue.kro.kr/application/48",
+    googleFormLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLScBmssFZc5Rxxj5YLxxP2Mq0EKAphbeNCPKBl48XPGWUGFpZQ/viewform",
   },
   {
     post: "디자이너.md",
     title: "Product Designer",
     position: "디자이너",
-    googleFormLink: "https://www.simblue.kro.kr/application/48",
+    googleFormLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLScBmssFZc5Rxxj5YLxxP2Mq0EKAphbeNCPKBl48XPGWUGFpZQ/viewform",
   },
 ];
 
